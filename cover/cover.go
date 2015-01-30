9baed70786c1f2009958f3331177c7b811f52752
James Duncombe <james@jamesduncombe.com>
2015-01-30 20:38:36 +0100

    Go fmt tidying

--- cover/cover.go
+++ cover/cover.go
@@ -3,15 +3,15 @@
 package cover
 
 import (
-  "bytes"
-  "bufio"
-  "fmt"
-  "io"
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
 )
 
 var (
-  ID3  = []byte{ 0x49, 0x44, 0x33 }
-  APIC = []byte{ 0x41, 0x50, 0x49, 0x43 }
+	ID3  = []byte{0x49, 0x44, 0x33}
+	APIC = []byte{0x41, 0x50, 0x49, 0x43}
 )
 
 type Cover struct {
@@ -19,27 +19,27 @@ type Cover struct {
 
 // HasId3 returns whether the passed in bytestream contains what is an ID3 tag
 func HasId3(p []byte) bool {
-  return bytes.Equal(p, ID3)
+	return bytes.Equal(p, ID3)
 }
 
 func Id3Ver(br *bufio.Reader) {
-  ver := make([]byte, 2)
-  br.Read(ver)
-  fmt.Printf("ID3v2.%d Rev: %d\n", uint8(ver[0]), uint8(ver[1]))
+	ver := make([]byte, 2)
+	br.Read(ver)
+	fmt.Printf("ID3v2.%d Rev: %d\n", uint8(ver[0]), uint8(ver[1]))
 }
 
 func HasPicture(br *bufio.Reader) bool {
-  // find picture
-  for {
-    _, err := br.ReadBytes(APIC[0])
-    // if we hit the end then it didn't have a APIC tag :(
-    if err == io.EOF {
-      return false
-    }
-    // otherwise, peek 3 ahead
-    by, _ := br.Peek(3)
-    if ok := bytes.Equal(by, APIC[1:4]); ok {
-      return true
-    }
-  }
+	// find picture
+	for {
+		_, err := br.ReadBytes(APIC[0])
+		// if we hit the end then it didn't have a APIC tag :(
+		if err == io.EOF {
+			return false
+		}
+		// otherwise, peek 3 ahead
+		by, _ := br.Peek(3)
+		if ok := bytes.Equal(by, APIC[1:4]); ok {
+			return true
+		}
+	}
 }
